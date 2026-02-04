ecece4de297d823bc1b0cd4aad11c5caa2e22660
AideNaDa <152689555+AideNaDa@users.noreply.github.com>
2026-02-04 23:58:07 +0100

    Refactor cat information parsing and error handling

--- task_2.py
+++ task_2.py
@@ -6,19 +6,26 @@ def get_cats_info(path: str) -> list[dict]:
     :param path: path to the text file.
     :return: list of dictionaries, e.g., [{'id': '1', 'name': 'A', 'age': '3'}]
     """
-    cat_inf = []
+    cats_inf = []
     try:
         with open(path, 'r', encoding='utf-8') as file:
             for line in file:
                 if not line.strip():
                     continue
 
-                cat_id, name, age = line.strip().split(',')
-                cat_inf.append({'id': cat_id, 'name': name, 'age': age})
+           try:
+                    cat_id, name, age = line.strip().split(',')
+                    cats_inf.append({
+                        "id": cat_id,
+                        "name": name,
+                        "age": age
+                    })
+                except ValueError:
+                    continue  # skip malformed lines
 
-    except  FileNotFoundError as e:
-        print(f'{e}')
-    except Exception as e:
-        print(f"{e}")
+    except  FileNotFoundError:
+        return []
+    except Exception:
+        return []
 
-    return cat_inf
+    return cats_inf
